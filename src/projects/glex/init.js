12a5a0b8dd87e7a169636d842f7eaa9f07bc3b2e
vangheem <vangheem@gmail.com>
2017-09-04 10:06:15 -0500

    initial

---
+++ src/projects/glex/init.js
@@ -0,0 +1,18 @@
+(function(exports) {
+    'use strict';
+    //initialize the app
+    var settings = {
+        Model: JSONMediaModel,
+        PlayerView: PlayerView,
+        PlaylistView: PlaylistPlayerView,
+        dataURL: "https://glex.nathanvangheem.com/@firetv",
+        showSearch: true,
+        displayButtons:false,
+        skipLength: 10,
+        controlsHideTime: 3000,
+        networkTimeout: 20,
+        retryTimes: 3
+    };
+
+    exports.app = new App(settings);
+}(window));
